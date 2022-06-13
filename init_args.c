f50f2008f1c1a7a4445b7f642c4cde85554f5293
Ulugbek Isroilov <uisroilo@lab1r3s13.42abudhabi.ae>
2022-06-13 19:31:41 +0400

    arguments initailized

--- init_args.c
+++ init_args.c
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init_args.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: uisroilo <uisroilo@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/11 16:49:02 by uisroilo          #+#    #+#             */
-/*   Updated: 2022/06/13 18:14:07 by uisroilo         ###   ########.fr       */
+/*   Created: 2022/06/13 18:08:30 by uisroilo          #+#    #+#             */
+/*   Updated: 2022/06/13 18:18:17 by uisroilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
+void	ft_init_args(int argc, char **argv, t_prog **data)
 {
-	t_prog	*data;
-
-	data = NULL;
-	if (!(argc == 5 || argc == 6))
-	{
-		write(2, "Wrong arguments\n", 16);
-		return (0);
-	}
-	if (check_arguments(argc, argv))
-		return (1);
-	data = malloc(sizeof(t_prog));
-	ft_init_args(argc, argv, &data);
+	(*data)->philo_nums = ft_atoi(argv[1]);
+	(*data)->time_die = ft_atoi(argv[2]);
+	(*data)->time_eat = ft_atoi(argv[3]);
+	(*data)->time_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		(*data)->eat_nums = ft_atoi(argv[5]);
+	else
+		(*data)->eat_nums = 0;
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: uisroilo <uisroilo@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 16:47:55 by uisroilo          #+#    #+#             */
-/*   Updated: 2022/06/12 22:31:52 by uisroilo         ###   ########.fr       */
+/*   Updated: 2022/06/13 18:15:22 by uisroilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,20 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
 
-int	ft_atoi(const char *str);
-int	check_arguments(int argc, char **argv);
+typedef struct s_prog
+{
+	int	philo_nums;
+	int	time_die;
+	int	time_eat;
+	int	time_sleep;
+	int	time_sleep;
+	int	eat_nums;
+}	t_prog;
+
+int		ft_atoi(const char *str);
+int		check_arguments(int argc, char **argv);
+void	ft_init_args(int argc, char **argv, t_prog **data);
 
 #endif
